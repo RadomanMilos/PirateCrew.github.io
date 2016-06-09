64e9cd3194de03f32877ec2b0db6155ac5326134
TensaZangetsu <l.lawliet46@gmail.com>
2016-06-09 13:42:17 +0200

    Tets

---
+++ js/main.js
@@ -0,0 +1,12 @@
+   /*globals $:false */
+(function (global, $) {
+    'use strict';
+
+    $('header .fa-bars').click(function(){
+        $('header ul').fadeToggle();
+    });
+}(window, jQuery));
+    
+
+
+
